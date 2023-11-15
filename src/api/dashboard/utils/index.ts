0acac1b8e3376c299e3092bbf968a4a65f41b4f5
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-11-15 13:47:02 -0500

    create formatUsersForTable func

--- src/api/dashboard/utils/index.ts
+++ src/api/dashboard/utils/index.ts
@@ -3,6 +3,6 @@ export { default as createPersonalInformation } from "./createPersonalInformatio
 export { default as createMovieDetailsInformation } from "./createMovieDetailsInformation";
 export { default as createStreamingInformtion } from "./createStreamingInformation";
 export { default as createDummyDataJSON } from "./createDummyData";
-export { default as formatUsersForTable } from "./dashboardUtils/formatUsersForTable";
+export { default as formatUsersForTable } from "./formatUsersForTable";
 export { default as formatUserMoviesForTable } from "./dashboardUtils/formatUserMoviesForTable";
 export { default as colors } from "./colors";
