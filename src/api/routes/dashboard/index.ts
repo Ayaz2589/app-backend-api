30475b65722b2e43575c59ac38ca9636948c800a
Ayaz Uddin <ayazuddin@ayazs-air.mynetworksettings.com>
2023-11-15 14:10:39 -0500

    create dummy data get request

--- src/api/routes/dashboard/index.ts
+++ src/api/routes/dashboard/index.ts
@@ -1,8 +1,22 @@
 import express from "express";
 const router = express.Router();
 
+import { createDummyData } from "../../dashboard/utils";
+
 router.get("/status", (req, res) => {
   res.status(200).send("Dashboard API is running");
+});
+
+router.get("/get-dummy-data", (req, res) => {
+  const getDummyData = async () => {
+    try {
+      const response = await createDummyData();
+      res.status(200).json(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  getDummyData();
 });
 
 export default router;
